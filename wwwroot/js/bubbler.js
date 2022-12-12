d689acf3d0e5760e432f06e2d6f9405429289927
gutitayl <77405164+gutitayl@users.noreply.github.com>
2022-12-11 18:39:59 -0700

    Add files via upload

---
+++ wwwroot/js/bubbler.js
@@ -0,0 +1,37 @@
+﻿//alert("Hello")
+
+window.onload = function () {
+    //var bdy = document.getElementsByTagName("body")[0];
+
+    //for (ele of bdy.children) {
+    //    ele.onclick = wasClicked;
+    //    for (var chl of ele.children) {
+    //        chl.onclick = wasClicked;
+    //        for (var gchl of chl.children) {
+    //            gchl.onclick = wasClicked;
+    //            for (var ggchl of gchl.children) {
+    //                ggchl.onclick = wasClicked;
+    //            }
+    //        }
+    //    }
+    //}
+
+    var div = document.getElementById("myDiv");
+
+    div.onclick = wasClicked;
+};
+
+function wasClicked(e) {
+    e = e || window.event;
+    //alert(e);
+    //alert(e.target);
+    var clicked = e.target;
+    if (clicked.className == "clicked") {
+        clicked.className = "";
+    } else {
+        clicked.className = "clicked";
+    }
+
+    //alert(this.id + "Was Clicked");
+}
+
---
+++ wwwroot/js/events.js
@@ -0,0 +1,34 @@
+﻿//alert("Hello");
+window.onload = whenPageLoads;
+
+function whenPageLoads() {
+    var btnOne = document.getElementById("btnOne")
+    var btnTwo = document.getElementById("btnTwo")
+    var btnThree = document.getElementById("btnThree")
+    var btnFour = document.getElementById("btnFour")
+    var btnFive = document.getElementById("btnFive")
+
+
+    //alert(btnOne + "out");
+
+    btnOne.onclick = funOne;
+    btnTwo.onclick = funOne;
+    btnThree.onclick = function () {
+        alert("Button 3 was clicked.")
+    };
+    btnFour.onmouseenter = function () {
+        this.style.backgroundColor = "#f00"
+    };
+
+    btnFour.onmouseleave = function () {
+        this.style.backgroundColor = "#0f0"
+    };
+    btnFive.onclick = funOne;
+
+
+    
+}
+
+function funOne() {
+    alert(this.id + " was clicked")
+}
